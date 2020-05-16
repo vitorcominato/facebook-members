fdb9234096ed9e0915e00b24903be491257268a6
Vitor Amorim <vitor.cominato@gmail.com>
2020-05-16 13:34:13 -0300

    Adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "react/prop-types": "off",
+    "linebreak-style": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "jsx-a11y/label-has-for": "off",
+  },
+  "globals": {
+    "window": true,
+    "navigator": true,
+    "fetch": true,
+    "localStorage": true,
+    "document": true,
+    "isNaN": true,
+    "FormData": true,
+    "FileReader": true,
+    "atob": true,
+    "Blob": true,
+  }
+};
