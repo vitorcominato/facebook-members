7a8590f66def5e1bfbdc5c72dba50f26042ff6b0
Vitor Amorim <vitor.cominato@gmail.com>
2020-05-16 16:11:17 -0300

    Finishing User info page

--- src/pages/UserInfo/UserInfo.jsx
+++ src/pages/UserInfo/UserInfo.jsx
@@ -1,27 +1,57 @@
 /**
  *
- * User Info
+ * User Info Page
  *
  *
  */
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import {
+  Card,
+  CardHeader,
+  Avatar,
+  CardMedia,
+  CardContent,
+} from '@material-ui/core';
 
-// import getUserInfo from '../../actions/githubActions';
+import * as gitHubActions from '../../actions/githubActions';
 import PageContent from '../../containers/PageContent/PageContent';
 
-function UserInfo() {
+function UserInfo(props) {
+  const userInfo = useSelector(state => state.gitHubReducer.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // dispatch(getUserInfo());
+    const { match } = props;
+    const { login } = match.params;
+    dispatch(gitHubActions.getUserInfo(login));
   }, [dispatch]);
-
 
   return (
     <div className="user-info" id="user-info">
       <PageContent page="user-info">
-        <span>Teste</span>
+        <Link to="/">Voltar</Link>
+        <Card>
+          <CardHeader
+            avatar={
+              (
+                <Avatar alt={userInfo.login} src={userInfo.avatar_url} />
+              )
+            }
+            title={userInfo.name}
+            subheader={`Github user since ${moment(userInfo.created_at).format('DD/MM/YYYY')}`}
+          />
+          <CardMedia
+            image="/static/images/cards/paella.jpg"
+            title="Paella dish"
+          />
+          <CardContent>
+            <p>{`Number of repositories: ${userInfo.public_repos}`}</p>
+            <p>{`Number of followers: ${userInfo.followers}`}</p>
+          </CardContent>
+        </Card>
       </PageContent>
     </div>
   );
